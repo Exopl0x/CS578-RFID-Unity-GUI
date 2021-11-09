48d0435747297afb8ce65c6662d5a7f3cc421eec
Tamarind0 <78615030+Tamarind0@users.noreply.github.com>
2021-11-09 11:20:10 -0800

    Basic main scene set up

---
+++ Assets/Script/Connect_To_Serial_Port.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Connect_To_Serial_Port : MonoBehaviour
+{
+    public TMP_InputField com_port;
+    public TMP_InputField password;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
