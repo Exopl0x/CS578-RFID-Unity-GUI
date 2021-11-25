e8747d78a29d6c45f824a9929a74d4f80050dc7a
Tamarind0 <78615030+Tamarind0@users.noreply.github.com>
2021-11-24 21:52:11 -0800

    Setup the script for serial comm

--- Assets/Script/Connect_To_Serial_Port.cs
+++ Assets/Script/Connect_To_Serial_Port.cs
@@ -9,10 +9,12 @@ public class Connect_To_Serial_Port : MonoBehaviour
 {
     public TMP_InputField com_port;
     public TMP_InputField password;
+
+     public SerialController serialController;
     // Start is called before the first frame update
     void Start()
     {
-
+        serialController = GameObject.Find("SerialController").GetComponent<SerialController>();
         
     }
 
